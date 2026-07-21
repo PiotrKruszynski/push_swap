edac5cb5e7d17e5aa759c6bc8190b6121824b02d
Piotr Kruszynski <pkruszyn@c2r6s1.42warsaw.pl>
2026-07-21 12:28:01 +0200

    rrotate is alive

--- srcs/ops_rrotate.c
+++ srcs/ops_rrotate.c
@@ -1,21 +1,36 @@
 #include "push_swap.h"
 
-static void	do_reverse_rotate(t_stack *stack)
+static void	reverse_rotate(t_stack *stack)
 {
-	
+	t_node		*last;
+
+	if (!stack || !stack->top || !stack->top->next)
+		return ;
+	last = stack->bottom;
+	stack->bottom = last->prev;
+	stack->bottom->next = NULL;
+	last->prev = NULL;
+	last->next = stack->top;
+	stack->top->prev = last;
+	stack->top = last;
 }
 
 void	rra(t_ps *ps)
 {
-	
+	reverse_rotate(&(ps->a));
+	write(1, "rra\n", 4);
 }
 
 void	rrb(t_ps *ps)
 {
-
+	reverse_rotate(&(ps->b));
+	write(1, "rrb\n", 4);
 }
 
 void	rrr(t_ps *ps)
 {
-
+	reverse_rotate(&(ps->a));
+	reverse_rotate(&(ps->b));
+	write(1, "rrr\n", 4);
 }
+
