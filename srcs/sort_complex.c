b35ae3620fde78a7a196fb214be5fbb9e26b5e44
Kinga Wlodarska <281594250+Kinga-Wlodarska@users.noreply.github.com>
2026-07-30 00:05:11 +0200

    poprawiony sort_medium

--- srcs/sort_complex.c
+++ srcs/sort_complex.c
@@ -1,7 +1,5 @@
 #include "push_swap.h"
 
-//	QuickSort -mod
-
 void	sort_complex(t_ps *ps)
 {
 	sort_simple(ps);
--- srcs/sort_medium.c
+++ srcs/sort_medium.c
@@ -52,7 +52,7 @@ static int	find_pos_b(t_stack *stack, int target_index)
 	return (-1);
 }
 
-static void	cheap_push_back(t_ps *ps, int pos_max, int pos_next)
+static void	cheap_push_back(t_ps *ps, int pos_max, int pos_next, int *target_max)
 {
 	int	cost_max;
 	int	cost_next;
@@ -63,14 +63,16 @@ static void	cheap_push_back(t_ps *ps, int pos_max, int pos_next)
 	{
 		rotate_b_to_top(ps, pos_next);
 		pa(ps);
-		rotate_b_to_top(ps, find_pos_b(&(ps->b), ps->b.size - 1));
+		rotate_b_to_top(ps, find_pos_b(&(ps->b), *target_max));
 		pa(ps);
 		sa(ps);
+		*target_max -= 2;
 	}
 	else
 	{
 		rotate_b_to_top(ps, pos_max);
 		pa(ps);
+		*target_max -= 1;
 	}
 }
 
@@ -78,32 +80,37 @@ static void	push_back_to_a(t_ps *ps)
 {
 	int	pos_max;
 	int	pos_next;
+	int	target_max;
 
+	target_max = ps->b.size - 1;
 	while (ps->b.size > 0)
 	{
-		pos_max = find_pos_b(&(ps->b), ps->b.size - 1);
-		if (ps->b.size > 1)
-			pos_next = find_pos_b(&(ps->b), ps->b.size - 2);
+		pos_max = find_pos_b(&(ps->b), target_max);
+		if (ps->b.size > 1 && target_max > 0)
+			pos_next = find_pos_b(&(ps->b), target_max - 1);
 		else
 			pos_next = -1;
-		cheap_push_back(ps, pos_max, pos_next);
+		cheap_push_back(ps, pos_max, pos_next, &target_max);
 	}
 }
 
-static void	push_to_b(t_ps *ps, int *counter, int chunk_size)
+static void	push_to_b(t_ps *ps, int min_chunk, int max_chunk)
 {
-	while (ps->a.size > 0)
+	int	size_to_push;
+	
+	size_to_push = max_chunk - min_chunk + 1;
+	while (size_to_push > 0 && ps->a.size > 0)
 	{
-		if (ps->a.top->index <= *counter)
+		if (ps->a.top->index >= min_chunk && ps->a.top->index <= max_chunk)
 		{
-			pb(ps);
-			rb(ps);
-			(*counter)++;
-		}
-		else if (ps->a.top->index < (*counter + chunk_size))
-		{
-			pb(ps);
-			(*counter)++;
+			if (ps->a.top->index <= (min_chunk + max_chunk) / 2)
+			{
+				pb(ps);
+				rb(ps);
+			}
+			else
+				pb(ps);
+			size_to_push--;
 		}
 		else
 			ra(ps);
@@ -112,14 +119,25 @@ static void	push_to_b(t_ps *ps, int *counter, int chunk_size)
 
 static void	divide_chunks(t_ps *ps)
 {
-	int	counter;
-	int	calculated_chunk;
+	int	chunk_size;
+	int	min_chunk;
+	int	max_chunk;
+	int	total_size;
 
-	counter = 0;
-	calculated_chunk = get_chunk_size(ps->a.size);
-	push_to_b(ps, &counter, calculated_chunk);
+	total_size = ps->a.size;
+	chunk_size = get_chunk_size(total_size);
+	min_chunk = 0;
+	while (min_chunk < total_size)
+	{
+		max_chunk = min_chunk + chunk_size - 1;
+		if (max_chunk >= total_size)
+			max_chunk = total_size - 1;
+		push_to_b(ps, min_chunk, max_chunk);
+		min_chunk += chunk_size;
+	}
 	push_back_to_a(ps);
 }
+
 
 void	sort_medium(t_ps *ps)
 {
